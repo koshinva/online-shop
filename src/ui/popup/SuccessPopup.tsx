bf5337d1aedc502dd2d7376a38e32db28a32f51a
Koshin Valery <koshinva1993@gmail.com>
2023-04-15 08:56:45 +0300

    feat: сделать возможность закрытия попапа

--- src/ui/popup/SuccessPopup.tsx
+++ src/ui/popup/SuccessPopup.tsx
@@ -3,14 +3,16 @@ import { FC } from 'react';
 import './Popup.css';
 import Icon from 'ui/Icon';
 import { useTypedSelector } from 'hooks/useTypedSelector';
+import { useActions } from 'hooks/useActions';
 
 const SuccessPopup: FC = () => {
   const { isOpenedPopup } = useTypedSelector((state) => state.cart);
+  const { closePopup } = useActions();
   if (!isOpenedPopup) return null;
   return (
     <div className="popup_success">
       <p>Заказ успешно оформлен</p>
-      <button className="popup_success__button" type="button">
+      <button className="popup_success__button" type="button" onClick={() => closePopup()}>
         <Icon name="AiOutlineClose" />
       </button>
     </div>
