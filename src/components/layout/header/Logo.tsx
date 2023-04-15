f4c9440515246e399f9b0220affa21b48de617e4
Koshin Valery <koshinva1993@gmail.com>
2023-04-15 08:58:43 +0300

    feat: сделать логотип сайта ссылкой на главную

--- src/components/layout/header/Logo.tsx
+++ src/components/layout/header/Logo.tsx
@@ -1,11 +1,14 @@
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
 import Icon from 'ui/Icon';
 
 const Logo: FC = () => {
   return (
     <div className="logo">
-      <Icon className="fill-sky-900 text-4xl mr-2" name="AiOutlineShopping" />
-      <h1 className="logo__title">online-shop</h1>
+      <Link className="logo__link" to="/">
+        <Icon className="fill-sky-900 text-4xl mr-2" name="AiOutlineShopping" />
+        <h1 className="logo__title">online-shop</h1>
+      </Link>
     </div>
   );
 };
